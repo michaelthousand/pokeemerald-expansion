5eebc113714830eb600ded9efbe38eae99d69326
Michael Thousand <michael.thousand@outlook.com>
2025-08-03 14:27:30 -0500

    1st Push
    Added test maps, changed starters

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1157,3 +1157,7 @@ EventScript_VsSeekerChargingDone::
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
 	.include "data/scripts/dexnav.inc"
+
+	.include "data/maps/TestMapA/scripts.inc"
+
+	.include "data/maps/Test_Route/scripts.inc"
