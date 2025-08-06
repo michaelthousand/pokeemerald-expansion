e1eb9a45a725a1fe51cecca06c4353a30989ed8c
Michael Thousand <michael.thousand@outlook.com>
2025-08-06 11:57:15 -0500

    trainer battles
    Added May single battle test and Roxanne double  battle test

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -86,8 +86,8 @@
 #define FLAG_UNUSED_0x048    0x48 // Unused Flag
 #define FLAG_UNUSED_0x049    0x49 // Unused Flag
 #define FLAG_UNUSED_0x04A    0x4A // Unused Flag
-#define FLAG_UNUSED_0x04B    0x4B // Unused Flag
-#define FLAG_UNUSED_0x04C    0x4C // Unused Flag
+#define FLAG_UNUSED_0x04B    0x4B // Unused Flag // currently used for Roxanne test battle
+#define FLAG_UNUSED_0x04C    0x4C // Unused Flag // currently used for May test battle
 #define FLAG_UNUSED_0x04D    0x4D // Unused Flag // currently used for test lab pokeball 3
 #define FLAG_UNUSED_0x04E    0x4E // Unused Flag // currently used for test lab pokeball 2
 #define FLAG_UNUSED_0x04F    0x4F // Unused Flag // currently used for test lab pokeball 1
